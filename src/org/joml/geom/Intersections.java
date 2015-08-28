0f531463fde617630d046cbe6a8989a245bd047f
Longor <lars195@yahoo.de>
2015-08-29 00:03:28 +0200

    Added ray/xxx-methods to the Intersections class.

--- src/org/joml/geom/Intersections.java
+++ src/org/joml/geom/Intersections.java
@@ -50,4 +50,85 @@ public class Intersections {
 		return culler.isSphereInsideFrustum(sphere.centerX, sphere.centerY, sphere.centerZ, sphere.radius);
 	}
 	
+	/** Warning: Not yet tested. **/
+	public static final float intersectRayWithPlane(Rayf ray, Vector3f normal, Vector3f point) {
+		// unwrap ray onto stack
+		float rayDirX = ray.directionX;
+		float rayDirY = ray.directionY;
+		float rayDirZ = ray.directionZ;
+		float rayOrgX = ray.originX;
+		float rayOrgY = ray.originY;
+		float rayOrgZ = ray.originZ;
+		
+		// -((normal.dot(ray.origin) + (-normal.dot(Point))) / normal.dot(ray.direction));
+		
+		// float : NDR = NORMAL dot RAYDIR
+		float NDR = normal.x*rayDirX + normal.y*rayDirY + normal.z*rayDirZ;
+		
+		// float : NNDP = negate (NORMAL dot POINT)
+		float NNDP = -(normal.x*point.x + normal.y*point.y + normal.z*point.x);
+		
+		// float : NDO = NORMAL dot RAYPOS
+		float NDO = normal.x*rayOrgX + normal.y*rayOrgY + normal.z*rayOrgZ;
+		
+		// float : RET = -((NDO + NNDP) / NDR)
+		return -((NDO + NNDP) / NDR);
+	}
+	
+	/** Warning: Not yet tested. **/
+	public static final float intersectRayWithDisk(Rayf ray, Vector3f n, Vector3f p0, float radius) {
+		float planeIntersect = intersectRayWithPlane(ray, n, p0);
+		
+		if (planeIntersect < Float.POSITIVE_INFINITY) {
+			float pX = ray.originX + ray.directionX*planeIntersect;
+			float pY = ray.originY + ray.directionY*planeIntersect;
+			float pZ = ray.originZ + ray.directionZ*planeIntersect;
+			
+			float vX = pX - p0.x;
+			float vY = pY - p0.y;
+			float vZ = pZ - p0.z;
+			
+			float d2 = vX*vX+vY*vY+vZ*vZ;
+			
+			return (d2 <= radius*radius) ? planeIntersect : Float.POSITIVE_INFINITY;
+		}
+		
+		return Float.POSITIVE_INFINITY;
+	}
+	
+	public static final float intersectRayWithSphere(Rayf ray, Spheref sphere) {
+		return intersectRayWithSphere(ray, sphere.centerX, sphere.centerY, sphere.centerZ, sphere.radius);
+	}
+	
+	/** Warning: Not yet tested. **/
+	public static final float intersectRayWithSphere(Rayf ray, float centerX, float centerY, float centerZ, float radius) {
+		// unwrap ray onto stack
+		float rayOrgX = ray.originX;
+		float rayOrgY = ray.originY;
+		float rayOrgZ = ray.originZ;
+		float rayDirX = ray.directionX;
+		float rayDirY = ray.directionY;
+		float rayDirZ = ray.directionZ;
+		
+		float vX = centerX - rayOrgX;
+		float vY = centerY - rayOrgY;
+		float vZ = centerZ - rayOrgZ;
+		
+		float b = vX * rayDirX + vY * rayDirY + vZ * rayDirZ;
+		float vDot = vX*vX+vY*vY+vZ*vZ;
+		float disc = b*b - vDot + radius*radius;
+		
+		if (disc < 0)
+			return Float.POSITIVE_INFINITY;
+		
+		float d = (float) Math.sqrt(disc);
+		float t2 = b+d;
+		
+		if (t2 < 0)
+			return Float.POSITIVE_INFINITY;
+		
+		float t1 = b-d;
+		return (t1 > 0 ? t1 : t2);
+	}
+	
 }
